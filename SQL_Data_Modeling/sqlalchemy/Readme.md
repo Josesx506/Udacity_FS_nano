24a64e76a024dc181764c8b8d6ad1a0ce50e0ebd
Josesx506 <josesomojola@gmail.com>
2023-11-13 23:09:51 -0700

    Hello name flask app

--- SQL_Data_Modeling/sqlalchemy/Readme.md
+++ SQL_Data_Modeling/sqlalchemy/Readme.md
@@ -56,4 +56,13 @@ result.close()
 
 **ORM** is the highest layer of abstraction in SQLALchemy. It lets you compose SQL expressions by mapping python classes of objects to tables in the database, and wraps the `SQL Expressions` and `Engine` to work together to interact with the database.<br><br>
 
-With sqlalchemy, the entire `fyyur` website project can be written with python.
+With sqlalchemy, the entire `fyyur` website project can be written with python. <br><br>
+
+A flask app can be run with the following command `FLASK_APP=<path/to/python/script> flask run`. An example path is `sqlalchemy/flask_hello_app.py`. There should be no spaces between FLASK_APP and the equal to = sign. <br>
+Setting `FLASK_DEBUG=true` will enable debug mode which allows live reload.<br><br>
+A simple *hello_flask* app is created in this folder.
+- Use SQLAlchemy to connect to a local postgres database.
+- The app shows how to create a table with column constraints using SQLAlchemy ORM.
+- How to perform an INSERT transaction and commit the changes.
+- Perform a SELECT query on the database to retrieve the first row, and all the rows.
+- Use a flask decorator to print `Hello <name>` from the database into a local host address.
---
+++ SQL_Data_Modeling/sqlalchemy/flask_hello_app.py
@@ -0,0 +1,65 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+# Define configuration file for database connection. 
+# You can include an optional DBAPI e.g.  postgresql+psycopg2. psycopg2 is the default for postgres.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://josesmac:@localhost:5432/fyyur'
+# Use this line to silence the `SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead` warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Link database to flask app
+db = SQLAlchemy(app)
+
+# Create a class using db.model
+# By inheriting from db.Model, we map from our classes to tables via SQLAlchemy ORM
+class Person(db.Model):
+    # Specify the table name or allow sqlalchemy to set it to a lower-case version of your class's name
+    __tablename__ = 'persons'
+    # Define the column names
+
+    # This is the primary key column with integer type.
+    id = db.Column(db.Integer, primary_key=True)  
+
+    # This is the person's name class that is a string and NOT NULL
+    # Other constraints like unique can a;sp be enforced to maintain data integrity across columns
+    name = db.Column(db.String(), nullable=False)
+
+    # Additional contraints can also be enforced to avoid negative values in a columns
+    age = db.Column(db.Float, db.CheckConstraint('age>0'))
+
+    def __repr__(self):
+        '''
+        This function determines how the print statement represents a row of data from the class.
+        It is useful for debugging but it wanted me to include app.app_context()
+        '''
+        return f'<Person ID:{self.id}, name:{self.name}, age:{self.age}>'
+
+
+# As of Flask-SQLAlchemy 3.0, all access to db.engine (and db.session) requires an active Flask application context
+with app.app_context():
+    # Detects models and creates tables for the models (if they don't exist).
+    db.create_all()
+
+    # Perform INSERT transactions with sqlalchemy. 
+    # Note: The table has to be created before insert statements can be performed
+    p1 = Person(name='Jadin',age=45.0)
+    db.session.add(p1)
+    db.session.commit()
+
+# Other queries that can be executed are
+# Person.query.all() # Get all the rows
+# Person.query.filter(Person.name == 'Amy').first() # Perform a filter and select the first row
+
+@app.route('/')
+def index():
+    # Perform a query to retrieve the first row of the table that was manually inserted in sql
+    person = Person.query.first()
+    all = Person.query.all()        # Select all rows. Returns a list
+    return f'Hello, {person}'
+
+if __name__ == '__main__':
+   app.debug = True
+   app.run(host="0.0.0.0",port=5000)
