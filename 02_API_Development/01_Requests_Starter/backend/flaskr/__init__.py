7982d1576e1d248c1d5d04357e35dee7f968dba7
Josesx506 <josesomojola@gmail.com>
2023-11-27 00:04:46 -0700

    Create CORS api requests and handling errors

--- 02_API_Development/01_Requests_Starter/backend/flaskr/__init__.py
+++ 02_API_Development/01_Requests_Starter/backend/flaskr/__init__.py
@@ -8,6 +8,17 @@ from models import setup_db, Book
 
 BOOKS_PER_SHELF = 8
 
+def paginate_books(request, selection):
+    # Helper function that is used repeatedly
+    page = request.args.get("page", 1, type=int)
+    start = (page - 1) * BOOKS_PER_SHELF
+    end = start + BOOKS_PER_SHELF
+
+    books = [book.format() for book in selection]
+    current_books = books[start:end]
+
+    return current_books
+
 # @TODO: General Instructions
 #   - As you're creating endpoints, define them and then search for 'TODO' within the frontend to update the endpoints there.
 #     If you do not update the endpoints, the lab will not work - of no fault of your API code!
@@ -38,34 +49,155 @@ def create_app(test_config=None):
     #         update the frontend to handle additional books in the styling and pagination
     #         Response body keys: 'success', 'books' and 'total_books'
     # TEST: When completed, the webpage will display books including title, author, and rating shown as stars
-    @app.route('/books')
-    def get_books():
-        # Use `Pagination` to print out only the first 10 results. 
-        page = request.args.get('page', 1, type=int)
-        start = (page - 1) * 10
-        end = start + 8
-        books = Book.query.order_by(Book.id).all()
-        formatted_books = [book.format() for book in books]
+    @app.route("/books")
+    def retrieve_books():
+        selection = Book.query.order_by(Book.id).all()
+        # Use `Pagination` to print out only the first 8 books per page.
+        current_books = paginate_books(request, selection)
         
-        return jsonify({'success': True,
-                        'books': formatted_books[start:end], 
-                        'total_books': len(formatted_books)})
+        # Return a 404 error if the page number is invalid
+        if len(current_books) == 0:
+            abort(404)
+
+        return jsonify(
+            {
+                "success": True,
+                "books": current_books,
+                "total_books": len(Book.query.all()),
+            }
+        )
 
     # @TODO: Write a route that will update a single book's rating.
     #         It should only be able to update the rating, not the entire representation
     #         and should follow API design principles regarding method and route.
     #         Response body keys: 'success'
     # TEST: When completed, you will be able to click on stars to update a book's rating and it will persist after refresh
+    @app.route("/books/<int:book_id>", methods=["PATCH"])
+    def update_book(book_id):
+
+        body = request.get_json()
+
+        try:
+            book = Book.query.filter(Book.id == book_id).one_or_none()
+            if book is None:
+                abort(404)
+
+            # Check if the request has a 'rating' key and convert it from string to integer
+            if "rating" in body:
+                book.rating = int(body.get("rating"))
+
+            # Update the db
+            book.update()
+
+            # Alert the user about the id of the book that was changed
+            return jsonify({"success": True, "id": book.id})
+
+        except:
+            abort(400)
 
     # @TODO: Write a route that will delete a single book.
     #        Response body keys: 'success', 'deleted'(id of deleted book), 'books' and 'total_books'
     #        Response body keys: 'success', 'books' and 'total_books'
 
     # TEST: When completed, you will be able to delete a single book by clicking on the trashcan.
+    @app.route("/books/<int:book_id>", methods=["DELETE"])
+    def delete_book(book_id):
+        try:
+            book = Book.query.filter(Book.id == book_id).one_or_none()
+
+            if book is None:
+                abort(404)
+
+            book.delete()
+            selection = Book.query.order_by(Book.id).all()
+            # The pagination helps to refresh the number of books shown after each deletion
+            current_books = paginate_books(request, selection)
+
+            return jsonify(
+                {
+                    "success": True,
+                    "deleted": book_id,
+                    "books": current_books,
+                    "total_books": len(Book.query.all()),
+                }
+            )
+
+        except:
+            abort(422)
 
     # @TODO: Write a route that create a new book.
     #        Response body keys: 'success', 'created'(id of created book), 'books' and 'total_books'
     # TEST: When completed, you will be able to a new book using the form. Try doing so from the last page of books.
     #       Your new book should show up immediately after you submit it at the end of the page.
+    @app.route("/books", methods=["POST"])
+    def create_book():
+        body = request.get_json()
+
+        new_title = body.get("title", None)
+        new_author = body.get("author", None)
+        new_rating = body.get("rating", None)
+
+        try:
+            # Create the new book
+            book = Book(title=new_title, author=new_author, rating=new_rating)
+            # Insert it into the db
+            book.insert()
+
+            # Reload all the books for the home page view
+            selection = Book.query.order_by(Book.id).all()
+            current_books = paginate_books(request, selection)
+
+            return jsonify(
+                {
+                    "success": True,
+                    "created": book.id,
+                    "books": current_books,
+                    "total_books": len(Book.query.all()),
+                }
+            )
+
+        except:
+            abort(422)
+
+    # -----------------------------------------------------------------------------------------------------------------------------
+    # ------------------------------------- handling errors in the app instead of using abort -------------------------------------
+    # -----------------------------------------------------------------------------------------------------------------------------
+    @app.errorhandler(400)
+    def bad_request(error):
+        # If book cannot be updated
+        return jsonify({
+            "success": False, 
+            "error": 400,
+            "message": "bad request, update error"
+            }), 400  
+    
+    @app.errorhandler(404)
+    def not_found(error):
+        # If a book is not found in the db
+        return jsonify({
+            "success": False, 
+            "error": 404,
+            "message": "not found"
+            }), 404
+    
+    @app.errorhandler(405)
+    def not_allowed(error):
+        # If a book is not found in the db
+        # This can handle request like `curl -X POST http://127.0.0.1:5000/books/200`
+        # Because /books/<book_id> endpoint is defined to handle only PATCH and DELETE requests, it'll raise an error for POST.
+        return jsonify({
+            "success": False, 
+            "error": 405,
+            "message": "method not allowed"
+            }), 405
+    
+    @app.errorhandler(422)
+    def unprocessable(error):
+        # If a book cannot be created or delted from the db
+        return jsonify({
+            "success": False, 
+            "error": 422,
+            "message": "unprocessable"
+            }), 422
 
     return app
--- 02_API_Development/Readme.md
+++ 02_API_Development/Readme.md
@@ -31,14 +31,39 @@ The `00_API_Initial` has files with scripts on how to:
 - do pagination on the `plants` db. 
 <br>
 
-The remaining folders are for implementing the `Udacity bookshelf app` whichs uses the `books` db.<br><br>
+The remaining folders are for implementing the `Udacity bookshelf app` whichs uses the `books` db include
+- `01_Requests_Starter` for creating requests. I also updated it to handle errors instead of creating a new folder.
+
+<br>
+
+To run the react app, 
+```bash
+# Go to the frontend directory
+cd 01_Requests_Starter/frontend
+# Delete the package-lock.json file
+rm package-lock.json
+# Install jquery
+npm install jquery --save
+# Install additional npm modules to create a node_modules directory
+npm install
+# Fix errors 
+npm audit fix --force
+# Launch the server
+npm run start
+```
+<br><br>
 
 
 #### CURL
 - Use `Cmd+Shift+C` to go to Chrome developer tools in your browser or use the 3 buttons at the top right or use `Fn+F12`
 - Check if curl is installed with `curl --version`. The documentation help can be accessed with `curl --help`.
 - Test a url using curl with `curl -X POST <url_link>`. Specify *-X POST* lets curl know it's a post request otherwise it implementsa a GET request by default.
-- You can also pipe the results to json format using the command line **jq** processor e.g. `curl https://restcountries.com/v3.1/currency/cop | jq "."`. This makes the response easier to read. jq is installed as part of conda on my mac.<br><br>
+- You can also pipe the results to json format using the command line **jq** processor e.g. `curl https://restcountries.com/v3.1/currency/cop | jq "."`. This makes the response easier to read. jq is installed as part of conda on my mac.
+- The command to return all of the books - `curl http://127.0.0.1:5000/books`
+- The command to update a single book's rating - `curl http://127.0.0.1:5000/books/8 -X PATCH -H "Content-Type: application/json" -d '{"rating":"1"}'`
+- The command to delete a single book - `curl -X DELETE http://127.0.0.1:5000/books/8`
+- The command to create a new book - `curl -X POST -H "Content-Type: application/json" -d '{"title":"Neverwhere", "author":"Neil Gaiman", "rating":"5"}' http://127.0.0.1:5000/books`
+<br><br>
 
 
 #### Request Endpoints
@@ -52,6 +77,12 @@ su - postgres bash -c "createdb plants"
 su - postgres bash -c "psql < plantsdb_setup/plantsdb-setup.sql"
 su - postgres bash -c "psql plants < plantsdb_setup/plants.psql"
 ```
+<br><br>
+
+#### App routes
+- For performing requests - `@app.route("/<route_path_or_arguements>", methods=["<method_name>"])`
+- For handiling errors - `@app.errorhandler(<error_number>)`. This returns a json response.
+<br><br>
 
 #### Terminal Tips
 - Kill a process with 
@@ -59,3 +90,10 @@ su - postgres bash -c "psql plants < plantsdb_setup/plants.psql"
 ps -a
 sudo kill <pid>
 ```
+- Check if a port is already occuppied and kill the process
+```bash
+sudo su - 
+ps -ef | grep postmaster | awk '{print $2}'
+kill <PID> 
+```
+- Search whole word, recursively in a folder, case-insensitive, and display the files having the keyword `grep -wril "todo" ./frontend/src/`
