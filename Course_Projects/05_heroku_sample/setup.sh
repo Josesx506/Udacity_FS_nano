201507bd9d8d7b6f74d6505a054fe89d38a7cd36
Josesx506 <josesomojola@gmail.com>
2023-12-26 14:43:33 -0700

    env variable edited

--- Course_Projects/05_heroku_sample/setup.sh
+++ Course_Projects/05_heroku_sample/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATABASE_URL="postgres://admin:uz9QfTMhZC3CiwIDNhcqFp17UEmcoMgS@jr3iif.stackhero-network.com:5432/admin?sslmode=require"
+export DATABASE_URL="postgres://admin:<admin_password>@jr3iif.stackhero-network.com:5432/admin?sslmode=require"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
